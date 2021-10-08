397ed09f3a0dbbb1e84fee5c31137f87c8dbb8a0
DIR\pumin.wongsiri <pumin.wongsiri@accenture.com>
2021-10-08 21:24:29 +0700

    prevent users to click right on website

--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
 // prevent users to click right on website
-// document.addEventListener('contextmenu', (event) => event.preventDefault())
+document.addEventListener('contextmenu', (event) => event.preventDefault())
